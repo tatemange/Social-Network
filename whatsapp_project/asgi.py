429247b711048955943b628a836a3274a380e415
User <user@example.com>
2026-03-06 15:05:37 +0100

    first commit

--- whatsapp_project/asgi.py
+++ whatsapp_project/asgi.py
@@ -1,13 +1,15 @@
 import os
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whatsapp_project.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whatsapp_project.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
